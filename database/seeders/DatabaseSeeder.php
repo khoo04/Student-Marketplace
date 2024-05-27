c98ae421d8f1b9d6c32014302409345bf634d316
Khoo Zhen Xian <khoozhenxian@gmail.com>
2024-05-28 00:55:26 +0800

    Bug Fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(CategorySeeder::class);
         // \App\Models\User::factory(10)->create();
         $this->call([
             CategorySeeder::class,
