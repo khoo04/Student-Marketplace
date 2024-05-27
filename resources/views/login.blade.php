e56bfb08679dfa516b7f9b75b6887ab204bc11eb
Khoo Zhen Xian <khoozhenxian@gmail.com>
2024-05-27 16:30:26 +0800

    Change login page css

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -16,18 +16,52 @@
     @csrf
     <div class="field-container">
         <label for="email">Email</label>
-        <input type="email" id="email" name="email" required>
+        <div class="input-box">
+            <input type="text" id="email" name="email" required>
+        </div>
     </div>
     <div class="field-container">
         <label for="password">Password</label>
-        <input type="password" name="password" id="password" required>
+        <div class="input-box">
+            <input type="password" name="password" id="password" required>
+            <i class="fa-solid fa-eye-slash"></i>
+        </div>
         @error('email')
-        <p class="error-message">{{$message}}</p>
+        <p class="form-error-message">{{$message}}</p>
         @enderror
     </div>
     <button type="submit" class="submit-btn">LOGIN</button>
     <p class="register-link">Don't have an account? <a href="/register">Sign Up</a></p>
-
     </form>
 </div>
+@endsection
+
+@section('js')
+<script>
+$(document).ready(function(){
+
+    $("input").on("focus",function(){
+      $(this).parent().addClass("focused");
+    })
+
+    $("input").on("blur",function(){
+      $(this).parent().removeClass("focused");
+    })
+
+    $(".input-box i").on("mousedown", function(event) {
+      event.preventDefault();
+      event.stopPropagation();
+    })
+    .on("click", function() {
+      let passwordInput = $(this).prev("input");
+      if (passwordInput.attr("type") === "password") {
+          passwordInput.attr("type", "text");
+          $(this).removeClass("fa-eye-slash").addClass("fa-eye");
+      } else {
+          passwordInput.attr("type", "password");
+          $(this).removeClass("fa-eye").addClass("fa-eye-slash");
+      }
+  });
+});
+</script>
 @endsection
